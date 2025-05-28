6fddd97d10516a26f265893269b02ed68751117c
OldSchoolNewWorld <dreid02893@gmail.com>
2025-05-28 14:30:15 -0400

    Updated ToStringAiBC_StandardForm_WorksAsExpected and similar process.

--- OSNW-Numerics/Numeric-Tests/Comp-UT.vb
+++ OSNW-Numerics/Numeric-Tests/Comp-UT.vb
@@ -25,9 +25,9 @@ Namespace Complex
 
         <Fact>
         Sub Constructor_WorksAsExpected()
-            Dim Z As New System.Numerics.Complex(125.125, -50.5)
+            Dim Z As New System.Numerics.Complex(125.125, -50.5625)
             Assert.Equal(125.125, Z.Real)
-            Assert.Equal(-50.5, Z.Imaginary)
+            Assert.Equal(-50.5625, Z.Imaginary)
         End Sub
 
     End Class ' General
@@ -35,10 +35,10 @@ Namespace Complex
     Public Class ToString
 
         <Theory>
-        <InlineData(125.125, 50.5, "<125.125; 50.5>")>
-        <InlineData(-125.125, 50.5, "<-125.125; 50.5>")>
-        <InlineData(125.125, -50.5, "<125.125; -50.5>")>
-        <InlineData(-125.125, -50.5, "<-125.125; -50.5>")>
+        <InlineData(125.125, 50.5625, "<125.125; 50.5625>")>
+        <InlineData(-125.125, 50.5625, "<-125.125; 50.5625>")>
+        <InlineData(125.125, -50.5625, "<125.125; -50.5625>")>
+        <InlineData(-125.125, -50.5625, "<-125.125; -50.5625>")>
         <InlineData(Single.MaxValue, Single.MinValue, ' Encourage exponential notation.
                     "<3.4028234663852886E+38; -3.4028234663852886E+38>")>
         Sub ToString_Generic_WorksAsExpected(
@@ -75,75 +75,135 @@ Namespace Complex
 
         End Sub
 
-        <Fact>
-        Sub ToStringAiBC_StandardForm_WorksAsExpected()
+        '<Fact>
+        'Sub ToStringAiBC_StandardForm_WorksAsExpected()
 
-            Dim Cplx As New System.Numerics.Complex(12.1, -15.4)
+        '    Dim Cplx As New System.Numerics.Complex(12.1, -15.4)
 
-            Dim S1 As String = $"Format AiBC0: " +
-                $"{String.Format(New ComplexStandardFormatter(), "{0:AiBC0}", Cplx)}"
-            Dim S2 As String = $"Format AiBC3: " +
-                $"{String.Format(New ComplexStandardFormatter(), "{0:AiBC3}", Cplx)}"
+        '    Dim OutStr As String = $"Format AiBC0: " +
+        '        $"{String.Format(New ComplexStandardFormatter(), "{0:AiBC0}", Cplx)}"
+        '    Dim S2 As String = $"Format AiBC3: " +
+        '        $"{String.Format(New ComplexStandardFormatter(), "{0:AiBC3}", Cplx)}"
 
-            Dim Expect1 As String = "Format AiBC0: 12-i15"
-            Dim Expect2 As String = "Format AiBC3: 12.100-i15.400"
+        '    Dim Expect1 As String = "Format AiBC0: 12-i15"
+        '    Dim Expect2 As String = "Format AiBC3: 12.100-i15.400"
 
-            Assert.Equal(Expect1, S1)
-            Assert.Equal(Expect2, S2)
+        '    Assert.Equal(Expect1, OutStr)
+        '    Assert.Equal(Expect2, S2)
 
-        End Sub
+        'End Sub
 
-        <Fact>
-        Sub ToStringAiBO_StandardForm_WorksAsExpected()
+        <Theory>
+        <InlineData(125.125, -50.5625, 0, "125-i51")>
+        <InlineData(-125.125, 50.5625, 3, "-125.125+i50.562")>
+        Sub ToStringAiBC_StandardForm_WorksAsExpected(ByVal real As Double, ByVal imaginary As Double,
+                                                      ByVal precision As UInt16, ByVal expected As String)
 
-            Dim Cplx As New System.Numerics.Complex(12.1, -15.4)
+            Dim Cplx As New System.Numerics.Complex(real, imaginary)
+            Dim FmtStr As String = $"{{0:AiBC{precision}}}"
 
-            Dim S1 As String = $"Format AiBO0: " +
-                $"{String.Format(New ComplexStandardFormatter(), "{0:AiBO0}", Cplx)}"
-            Dim S2 As String = $"Format AiBO3: " +
-                $"{String.Format(New ComplexStandardFormatter(), "{0:AiBO3}", Cplx)}"
+            Dim OutStr As String = $"{String.Format(New ComplexStandardFormatter(), FmtStr, Cplx)}"
 
-            Dim Expect1 As String = "Format AiBO0: 12 - i15"
-            Dim Expect2 As String = "Format AiBO3: 12.100 - i15.400"
-
-            Assert.Equal(Expect1, S1)
-            Assert.Equal(Expect2, S2)
+            Assert.Equal(expected, OutStr)
 
         End Sub
 
-        <Fact>
-        Sub ToStringABiC_StandardForm_WorksAsExpected()
+        '<Fact>
+        'Sub ToStringAiBO_StandardForm_WorksAsExpected()
 
-            Dim Cplx As New System.Numerics.Complex(12.1, -15.4)
+        '    Dim Cplx As New System.Numerics.Complex(12.1, -15.4)
 
-            Dim S1 As String = $"Format ABiC0: " +
-                $"{String.Format(New ComplexStandardFormatter(), "{0:ABiC0}", Cplx)}"
-            Dim S2 As String = $"Format ABiC3: " +
-                $"{String.Format(New ComplexStandardFormatter(), "{0:ABiC3}", Cplx)}"
+        '    Dim S1 As String = $"Format AiBO0: " +
+        '        $"{String.Format(New ComplexStandardFormatter(), "{0:AiBO0}", Cplx)}"
+        '    Dim S2 As String = $"Format AiBO3: " +
+        '        $"{String.Format(New ComplexStandardFormatter(), "{0:AiBO3}", Cplx)}"
 
-            Dim Expect1 As String = "Format ABiC0: 12-15i"
-            Dim Expect2 As String = "Format ABiC3: 12.100-15.400i"
+        '    Dim Expect1 As String = "Format AiBO0: 12 - i15"
+        '    Dim Expect2 As String = "Format AiBO3: 12.100 - i15.400"
 
-            Assert.Equal(Expect1, S1)
-            Assert.Equal(Expect2, S2)
+        '    Assert.Equal(Expect1, S1)
+        '    Assert.Equal(Expect2, S2)
+
+        'End Sub
+
+        <Theory>
+        <InlineData(125.125, -50.5625, 0, "125 - i51")>
+        <InlineData(-125.125, 50.5625, 3, "-125.125 + i50.562")>
+        Sub ToStringAiBO_StandardForm_WorksAsExpected(ByVal real As Double, ByVal imaginary As Double,
+                                                      ByVal precision As UInt16, ByVal expected As String)
+
+            Dim Cplx As New System.Numerics.Complex(real, imaginary)
+            Dim FmtStr As String = $"{{0:AiBO{precision}}}"
+
+            Dim OutStr As String = $"{String.Format(New ComplexStandardFormatter(), FmtStr, Cplx)}"
+
+            Assert.Equal(expected, OutStr)
 
         End Sub
 
-        <Fact>
-        Sub ToStringABiO_StandardForm_WorksAsExpected()
+        '<Fact>
+        'Sub ToStringABiC_StandardForm_WorksAsExpected()
 
-            Dim Cplx As New System.Numerics.Complex(12.1, -15.4)
+        '    Dim Cplx As New System.Numerics.Complex(12.1, -15.4)
 
-            Dim S1 As String = $"Format ABiO0: " +
-                $"{String.Format(New ComplexStandardFormatter(), "{0:ABiO0}", Cplx)}"
-            Dim S2 As String = $"Format ABiO3: " +
-                $"{String.Format(New ComplexStandardFormatter(), "{0:ABiO3}", Cplx)}"
+        '    Dim S1 As String = $"Format ABiC0: " +
+        '        $"{String.Format(New ComplexStandardFormatter(), "{0:ABiC0}", Cplx)}"
+        '    Dim S2 As String = $"Format ABiC3: " +
+        '        $"{String.Format(New ComplexStandardFormatter(), "{0:ABiC3}", Cplx)}"
 
-            Dim Expect1 As String = "Format ABiO0: 12 - 15i"
-            Dim Expect2 As String = "Format ABiO3: 12.100 - 15.400i"
+        '    Dim Expect1 As String = "Format ABiC0: 12-15i"
+        '    Dim Expect2 As String = "Format ABiC3: 12.100-15.400i"
 
-            Assert.Equal(Expect1, S1)
-            Assert.Equal(Expect2, S2)
+        '    Assert.Equal(Expect1, S1)
+        '    Assert.Equal(Expect2, S2)
+
+        'End Sub
+
+        <Theory>
+        <InlineData(125.125, -50.5625, 0, "125-51i")>
+        <InlineData(-125.125, 50.5625, 3, "-125.125+50.562i")>
+        Sub ToStringABiC_StandardForm_WorksAsExpected(ByVal real As Double, ByVal imaginary As Double,
+                                                      ByVal precision As UInt16, ByVal expected As String)
+
+            Dim Cplx As New System.Numerics.Complex(real, imaginary)
+            Dim FmtStr As String = $"{{0:ABiC{precision}}}"
+
+            Dim OutStr As String = $"{String.Format(New ComplexStandardFormatter(), FmtStr, Cplx)}"
+
+            Assert.Equal(expected, OutStr)
+
+        End Sub
+
+        '<Fact>
+        'Sub ToStringABiO_StandardForm_WorksAsExpected()
+
+        '    Dim Cplx As New System.Numerics.Complex(12.1, -15.4)
+
+        '    Dim S1 As String = $"Format ABiO0: " +
+        '        $"{String.Format(New ComplexStandardFormatter(), "{0:ABiO0}", Cplx)}"
+        '    Dim S2 As String = $"Format ABiO3: " +
+        '        $"{String.Format(New ComplexStandardFormatter(), "{0:ABiO3}", Cplx)}"
+
+        '    Dim Expect1 As String = "Format ABiO0: 12 - 15i"
+        '    Dim Expect2 As String = "Format ABiO3: 12.100 - 15.400i"
+
+        '    Assert.Equal(Expect1, S1)
+        '    Assert.Equal(Expect2, S2)
+
+        'End Sub
+
+        <Theory>
+        <InlineData(125.125, -50.5625, 0, "125 - 51i")>
+        <InlineData(-125.125, 50.5625, 3, "-125.125 + 50.562i")>
+        Sub ToStringABiO_StandardForm_WorksAsExpected(ByVal real As Double, ByVal imaginary As Double,
+                                                      ByVal precision As UInt16, ByVal expected As String)
+
+            Dim Cplx As New System.Numerics.Complex(real, imaginary)
+            Dim FmtStr As String = $"{{0:ABiO{precision}}}"
+
+            Dim OutStr As String = $"{String.Format(New ComplexStandardFormatter(), FmtStr, Cplx)}"
+
+            Assert.Equal(expected, OutStr)
 
         End Sub
 
